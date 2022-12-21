f0fd2efe51e41130d6c7f5f8748de8ec8f595b52
Alex Argunov <aleksandr_argunov@epam.com>
2022-12-21 03:46:52 +0300

    kit init

---
+++ test/clj/troglotit/topicsearch/core_test.clj
@@ -0,0 +1,8 @@
+(ns troglotit.topicsearch.core-test
+  (:require
+    [troglotit.topicsearch.test-utils :as utils]
+    [clojure.test :refer :all]))
+
+(deftest example-test
+  (is (= 1 2)))
+
---
+++ test/clj/troglotit/topicsearch/test_utils.clj
@@ -0,0 +1,15 @@
+(ns troglotit.topicsearch.test-utils
+  (:require
+    [troglotit.topicsearch.core :as core]
+    [integrant.repl.state :as state]))
+
+(defn system-state 
+  []
+  (or @core/system state/system))
+
+(defn system-fixture
+  []
+  (fn [f]
+    (when (nil? (system-state))
+      (core/start-app {:opts {:profile :test}}))
+    (f)))
